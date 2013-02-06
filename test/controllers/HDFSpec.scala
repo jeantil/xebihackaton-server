e5cd8f4dd8a289eacfe658ff7951dfe7b004172e
Jean Helou <jean.helou@gmail.com>
2013-02-06 11:25:32 +0100

    hdf5 init

---
+++ test/controllers/HDFSpec.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import org.specs2.mutable.Specification
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+
+class HDFSpec extends Specification{
+//  "An application" should {
+//    "be ok on index " in runningApplication {
+//      val result: Result = Application.index(FakeRequest().withSession("login"->"notempty"))
+//      1===1
+//    }
+//  }
+}
