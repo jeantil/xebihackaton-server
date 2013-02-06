228a8238b8eedaef67e120599c39a42247a2dc8a
Spark <pierre.gayvallet@gmail.com>
2013-02-06 15:58:07 +0100

    home page

--- public/js/HomeController.js
+++ public/js/HomeController.js
@@ -1,11 +1,62 @@
-function HomeController($scope, searchService, $location) {
 
-    $scope.selectedArtists = searchService.selectedArtists;
+function HomeController($scope, searchService, $http, $rootScope, $location) {
+
+    $rootScope.selectedArtists = searchService.selectedArtists;
 
     $scope.removeArtist = function (artistToRemove) {
-        $scope.selectedArtists = searchService.removeArtist(artistToRemove);
+        $rootScope.selectedArtists = searchService.removeArtist(artistToRemove);
     };
 
 
+    $scope.artistEvents = [];
+
+
+    var onArtistListChange = function() {
+
+        console.log("*** artist list change");
+
+        var newEvents = [];
+        _.each(searchService.selectedArtists, function(artist) {
+
+            var albumEvent = {
+                icon : "album",
+                artist : artist.name,
+                title : "New album announced",
+                date : "12/12/2013",
+                description : "New album : My Little album "
+            };
+
+            newEvents.push(albumEvent);
+
+            var concertEvent = {
+                icon : "concert",
+                artist : artist.name,
+                title : "World Tour 2013",
+                date : "12/12/2013",
+                description : artist.name + " is in your city the 11 November for a concert"
+            };
+
+            newEvents.push(concertEvent);
+
+        });
+
+        $scope.artistEvents = newEvents;
+
+
+    };
+    onArtistListChange();
+
+
+
+    $rootScope.$on("artists:add", onArtistListChange);
+    $rootScope.$on("artists:remove", onArtistListChange);
+
+    /*
+    $rootScope.$watch('selectedArtists', function(newValue, oldValue) {
+
+        console.log("selectedArtists changed", newValue, oldValue);
+
+    });
+    */
 
 }
