6a9bda81fc4c68112ebd61f1e340d08328e76665
Philippe Antoine <phil.antoine@gmail.com>
2013-02-06 14:54:49 +0100

    adding ponies

---
+++ public/js/vendor/traqball_alternative.js
@@ -0,0 +1,7 @@
+var replacementHTML= 	"<h1>Your browser doesn't support CSS 3d</h1>"+
+						"<iframe src='http://player.vimeo.com/video/16353784' width='400' height='300' frameborder='0'></iframe>"+
+						"<p>This is why you are seeing a video instead. If you want to have the \"real\" experience use "+
+						"<a href='http://www.google.com/chrome' 'title='download Chrome'>Chrome</a> or "+
+						"<a href='http://www.apple.com/safari/' 'title='download Safari'>Safari</a></p>";
+
+document.getElementById("stage").innerHTML = replacementHTML;
