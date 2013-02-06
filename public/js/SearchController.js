0e21465255c540fdf7e8fccc760dcaa969db0084
Benoit Lemoine <blemoine@xebia.fr>
2013-02-06 11:20:58 +0100

    Selected list

--- public/js/SearchController.js
+++ public/js/SearchController.js
@@ -1,6 +1,41 @@
 function SearchController($scope) {
     $scope.searchTerm = '';
 
+
+    $scope.addArtist = function(artistToAdd) {
+        var alreadyAdded = _.filter($scope.selectedArtists, function(artist) {
+            return artist.id == artistToAdd.id;
+        }).length > 0;
+        if(alreadyAdded) {
+            return;
+        }
+
+
+
+        $scope.selectedArtists.push(artistToAdd);
+    }
+
+    // TODO : ajax call to get list
+    $scope.selectedArtists = [
+        {
+            id : 1,
+            name: "Justin bieber",
+            position: {
+                lat: 36,
+                lng: 10
+            }
+        },
+        {
+            id : 2,
+            name: "Bob L'eponge",
+            position: {
+                lat: 36,
+                lng: 10
+            }
+        }
+
+    ];
+
     $scope.searchResults = [];
     var searchForTerm = function () {
         if ($scope.searchTerm) {
@@ -8,6 +43,7 @@ function SearchController($scope) {
             //TODO Faire un appel coté serveur
             $scope.searchResults = [
                 {
+                    id : 3,
                     name: "Bob Marley",
                     position: {
                         lat: 36,
