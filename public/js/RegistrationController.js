c9104eee331fb143ba2d4cb6e6624a647e08eb01
Benoit Lemoine <blemoine@xebia.fr>
2013-02-06 11:48:09 +0100

    Use debounce sur la registration

--- public/js/RegistrationController.js
+++ public/js/RegistrationController.js
@@ -17,48 +17,40 @@ function RegistrationController($scope, $location) {
 
     $scope.localisation = '';
 
-    //TODO remplacer par debounce
-    var timeouter = null;
     $scope.hasLocation = false;
-    $scope.$watch('localisation', function () {
-        if (timeouter) {
-            clearTimeout(timeouter);
-        }
+    var searchForLocation = function () {
+        geocoder.geocode({ 'address': $scope.localisation}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                var centerLocation = results[0].geometry.location;
 
-        timeouter = setTimeout(function () {
-            geocoder.geocode({ 'address': $scope.localisation}, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    var centerLocation = results[0].geometry.location;
+                var longitude = centerLocation.lng();
+                var latitude = centerLocation.lat();
+                $scope.center = {
+                    lat: latitude,
+                    lng: longitude
+                };
+                $scope.markers = [
+                    {
+                        latitude: parseFloat(latitude),
+                        longitude: parseFloat(longitude)
+                    }
+                ];
 
-                    var longitude = centerLocation.lng();
-                    var latitude = centerLocation.lat();
-                    $scope.center = {
-                        lat: latitude,
-                        lng: longitude
-                    };
-                    $scope.markers = [
-                        {
-                            latitude: parseFloat(latitude),
-                            longitude: parseFloat(longitude)
-                        }
-                    ];
-
-                    $scope.zoom = 14;
-                    $scope.hasLocation = true;
-                    $scope.$apply();
-                } else {
-                    console.log("Geocode was not successful for the following reason: " + status);
-                    $scope.hasLocation = false;
-                }
-            });
-        }, 1000);
-
-        $scope.register = function (isValid) {
-            if (isValid && $scope.hasLocation) {
-                $location.path('/search');
+                $scope.zoom = 14;
+                $scope.hasLocation = true;
+                $scope.$apply();
+            } else {
+                console.log("Geocode was not successful for the following reason: " + status);
+                $scope.hasLocation = false;
             }
-        }
+        });
+    };
 
-    });
+    $scope.$watch('localisation', _.debounce(searchForLocation, 1000));
+    $scope.register = function (isValid) {
+        if (isValid && $scope.hasLocation) {
+            $location.path('/search');
+        }
+    }
 
 }
